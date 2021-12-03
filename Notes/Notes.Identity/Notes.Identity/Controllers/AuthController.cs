02eeed5b9fcc2833fb617774a5731766bccc873e
Deimantas <Deimantas.Kukis@gmail.com>
2021-12-03 10:37:48 +0200

    controller created

---
+++ Notes/Notes.Identity/Notes.Identity/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Notes.Identity.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+    }
+}
