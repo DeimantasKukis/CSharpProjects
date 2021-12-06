65b0b610386448cf69f6eee1ceb61feaf8811b87
Deimantas <Deimantas.Kukis@gmail.com>
2021-12-06 15:26:23 +0200

    Register.cshtml created

--- Notes/Notes.Identity/Notes.Identity/Controllers/AuthController.cs
+++ Notes/Notes.Identity/Notes.Identity/Controllers/AuthController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Notes.Identity.Models;
-
+using System.Threading.Tasks;
 
 namespace Notes.Identity.Controllers
 {
@@ -47,6 +47,35 @@ namespace Notes.Identity.Controllers
                 return Redirect(viewModel.ReturnUrl);
             }
             ModelState.AddModelError(string.Empty, "Login error");
+            return View(viewModel);
+        }
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            var viewModel = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(returnUrl);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            var user = new AppUser
+            {
+                UserName = viewModel.Username
+            };
+            var result = await _userManager.CreateAsync(user, viewModel.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Redirect(viewModel.ReturnUrl);
+            }
+            ModelState.AddModelError(string.Empty, "Error occurred");
             return View(viewModel);
         }
     }
