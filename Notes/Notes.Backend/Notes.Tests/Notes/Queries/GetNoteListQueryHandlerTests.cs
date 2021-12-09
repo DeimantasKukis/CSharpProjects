0e7faa6dd2ee6baf5b03cfde1801e9f3b52fbd14
Deimantas <Deimantas.Kukis@gmail.com>
2021-12-09 11:02:14 +0200

    GetNoteListQueryHandlerTests created

--- Notes/Notes.Backend/Notes.Tests/Notes/Queries/GetNoteListQueryHandlerTests.cs
+++ Notes/Notes.Backend/Notes.Tests/Notes/Queries/GetNoteListQueryHandlerTests.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
+
 
 namespace Notes.Tests.Notes.Queries
 {
-    class GetNoteListQueryHandlerTests
+    public class GetNoteListQueryHandlerTests
     {
+        private readonly NotesDbContext Context;
+        private readonly IMapper Mapper;
+
+        public GetNoteListQueryHandlerTests(QueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+        [Fact]
+        public async Task GetNoteListQueryHandler_Success()
+        {
+            // Arrange
+            var handler = new GetNoteListQueryHandler(Context, Mapper);
+            //Act
+            var result = await handler.Handle(
+                new GetNoteListQuery
+                {
+                    UserId = NotesContextFactory.UserBId
+                }, CancellationToken.None);
+            // Assert
+            result.ShouldBeOfType<NoteListVm>();
+            result.Notes.Count.ShouldBe(2);
+        }
     }
 }
