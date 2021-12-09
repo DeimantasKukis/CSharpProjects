ed2a6a59e9896516ad3c64a28dd9229875334d8f
Deimantas <Deimantas.Kukis@gmail.com>
2021-12-09 10:10:15 +0200

    UpdateNoteCommandHandlerTests created

---
+++ Notes/Notes.Backend/Notes.Tests/Notes/Commands/UpdateNoteCommandHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Tests.Notes.Commands
+{
+    public class UpdateNoteCommandHandlerTests
+    {
+    }
+}
